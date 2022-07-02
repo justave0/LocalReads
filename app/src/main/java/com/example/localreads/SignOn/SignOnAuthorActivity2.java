4915cc8f25c00828c1c18b6b41911bbd17f72f16
Avery Avila <averyavila@fb.com>
2022-07-02 16:16:57 -0700

    Google Places Autocomplete

--- app/src/main/java/com/example/localreads/SignOn/SignOnAuthorActivity2.java
+++ app/src/main/java/com/example/localreads/SignOn/SignOnAuthorActivity2.java
@@ -17,7 +17,13 @@ import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.example.localreads.MainActivity;
 import com.example.localreads.Models.Author;
 import com.example.localreads.R;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.parse.ParseGeoPoint;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
@@ -27,6 +33,7 @@ import org.json.JSONObject;
 import org.parceler.Parcels;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import okhttp3.Headers;
@@ -34,9 +41,7 @@ import okhttp3.Headers;
 public class SignOnAuthorActivity2 extends AppCompatActivity {
     String TAG = "SignOnAuthorActivity2";
     EditText etSignOnBio;
-    EditText etAuthorSignOnCity;
-    EditText etAuthorSignOnState;
-    EditText etAuthorSignOnCountry;
+    LatLng authorLocation;
     Button btAuthorSignOn;
     ParseGeoPoint inputLocation;
 
@@ -46,38 +51,56 @@ public class SignOnAuthorActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_on_author2);
+        if (!Places.isInitialized()) {
+            Places.initialize(getApplicationContext(), getString(R.string.google_maps_api_key));
+        }
+        PlacesClient placesClient = Places.createClient(this);
         favoriteGenres = Parcels.unwrap(getIntent().getParcelableExtra("favoriteGenres"));
         etSignOnBio = findViewById(R.id.etSignOnBio);
-        etAuthorSignOnCity = findViewById(R.id.etAuthorSignOnCity);
-        etAuthorSignOnState = findViewById(R.id.etAuthorSignOnState);
-        etAuthorSignOnCountry = findViewById(R.id.etAuthorSignOnCountry);
+
         btAuthorSignOn = findViewById(R.id.btAuthorSignOn);
+
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+                getLatLng(place);
+            }
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
 
         btAuthorSignOn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findLocation();
+                createAuthor();
             }
         });
 
     }
-    // Checking if an author exists
-    // Use this code later for converting location to latlng
-    private void findLocation() {
-        String locationName = etAuthorSignOnCity.getText().toString() + "+" + etAuthorSignOnState.getText().toString() + "+" + etAuthorSignOnCountry.getText().toString();
-        locationName = locationName.replace(" ","+");
-        String geocode = getString(R.string.geocode_url) + "address=" +locationName + "&key=" + getString(R.string.google_maps_api_key);
+
+
+    public void getLatLng(Place place){
+        String placeHTTPS = "https://maps.googleapis.com/maps/api/place/details/json?placeid="+place.getId()+"&key="+ getString(R.string.google_maps_api_key);
         AsyncHttpClient client = new AsyncHttpClient();
-        client.get(geocode, new JsonHttpResponseHandler() {
+        client.get(placeHTTPS, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Headers headers, JSON json) {
                 Log.d(TAG,  "onSuccess");
                 JSONObject jsonObject = json.jsonObject;
                 try{
-                    JSONObject results = (JSONObject) jsonObject.getJSONArray("results").get(0);
-                    JSONObject location = results.getJSONObject("geometry").getJSONObject("location");
-                    inputLocation = new ParseGeoPoint( location.getDouble("lat"), location.getDouble("lng"));
-                    createAuthor();
+                    JSONObject results = (JSONObject) jsonObject.getJSONObject("result").getJSONObject("geometry").getJSONObject("location");
+                    inputLocation = new ParseGeoPoint(results.getDouble("lat"), results.getDouble("lng"));
+                    Log.i(TAG, inputLocation.toString());
                 }
                 catch (JSONException e){
                     Log.e(TAG, "Hit JSON exception", e);
@@ -87,13 +110,44 @@ public class SignOnAuthorActivity2 extends AppCompatActivity {
 
             @Override
             public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+
             }
         });
     }
 
+    // Checking if an author exists
+    // Use this code later for converting location to latlng
+//    private void findLocation() {
+//        String locationName = etAuthorSignOnCity.getText().toString() + "+" + etAuthorSignOnState.getText().toString() + "+" + etAuthorSignOnCountry.getText().toString();
+//        locationName = locationName.replace(" ","+");
+//        String geocode = getString(R.string.geocode_url) + "address=" +locationName + "&key=" + getString(R.string.google_maps_api_key);
+//        AsyncHttpClient client = new AsyncHttpClient();
+//        client.get(geocode, new JsonHttpResponseHandler() {
+//            @Override
+//            public void onSuccess(int statusCode, Headers headers, JSON json) {
+//                Log.d(TAG,  "onSuccess");
+//                JSONObject jsonObject = json.jsonObject;
+//                try{
+//                    JSONObject results = (JSONObject) jsonObject.getJSONArray("results").get(0);
+//                    JSONObject location = results.getJSONObject("geometry").getJSONObject("location");
+//                    inputLocation = new ParseGeoPoint( location.getDouble("lat"), location.getDouble("lng"));
+//                    createAuthor();
+//                }
+//                catch (JSONException e){
+//                    Log.e(TAG, "Hit JSON exception", e);
+//                    e.printStackTrace();
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+//            }
+//        });
+//    }
+
     private void createAuthor() {
         Author author = new Author();
-        author.setInputLocation( inputLocation);
+        author.setInputLocation(inputLocation);
         author.setBio(etSignOnBio.getText().toString());
         author.setBooks(new ArrayList<>());
         author.setLink("");
